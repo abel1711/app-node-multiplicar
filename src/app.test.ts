7c2cbc6a3b48943dc51d9d77d1d070a96adc12b8
Abel Aron Amieva <abelamieva@gmail.com>
2024-01-02 23:44:39 -0300

    refact

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,25 +1,19 @@
 import { ServerApp } from './presentation/server-app';
 
-
-
 describe('App test', () => {
 
     test('should be call ServerApp with custom values', async() => {
         const serverAppMock = jest.fn();
-
         ServerApp.run = serverAppMock;
         process.argv = ['node','app.ts', '-b', '8', '-l', '5', '-d', 'test-folder', '-n', 'test-file'];
-
         await import('./app');
-
         expect(serverAppMock).toHaveBeenCalledWith({
             base: 8,
             limit: 5,
             fileDestination: 'test-folder',
             fileName: 'test-file',
             showTable: false
-        })
+        });
+    });
 
-
-    })
-})
+});
